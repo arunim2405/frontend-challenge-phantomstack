1694fd0b3d5022fad20eb1fd9fd2eba70090654e
Arunim <arunim2@gmail.com>
2021-03-13 17:12:49 +0530

    functionality of table and search implemented

--- src/Components/PaginatedBooks.jsx
+++ src/Components/PaginatedBooks.jsx
@@ -1,26 +1,76 @@
 import React, { PureComponent } from 'react';
+import axios from "axios";
+import { DataGrid } from '@material-ui/data-grid';
+import TextField from '@material-ui/core/TextField';
 
 class PaginatedBooks extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            about: ""
+            isLoading:true,
+            data: [],
+            searchInput:""
         };
     }
     componentDidMount() {
+        try{
+            axios.get(" https://jsonplaceholder.typicode.com/posts", {
+              
+            }).then((res) => {
+                console.log(res);
+                this.setState({
+                    data: res.data,
+                    isLoading: false
+                })
+            });
+        }catch(e){
+            console.log(e)
+        }
+       
+    }
+    onSearchChange=(evt)=>{
+        console.log(evt.target.value)
         this.setState({
-            
+            searchInput: evt.target.value
         })
     }
     render() {
-        return (
+        console.log("DATA", this.state.data)
+        const columns=[
+        {   field:"id",
+            headerName:"S.No",
+           
+        },
+        {   field:"userId",
+        headerName:"ID",
+         },
+         {   field:"title",
+         headerName:"Title",
+         width:400
+          },
+          {   field:"body",
+          headerName:"Body",
+          width:800
+           },
 
-            <div className="row card">
-                <div className="col-md-12">
+
+        ]
+        return (
+            <div style={{ height: 700, width: '100%' }}>
+                {/* <div className="col-md-12">
                     <h3>About the Property</h3>
                    
-                </div>
-
+                </div> */}
+                <TextField value={this.state.searchInput} onChange={(evt)=>this.onSearchChange(evt)}/>
+                
+                <DataGrid rows={this.state.data} columns={columns} pageSize={10}
+                
+                filterModel={{
+                    items: [
+                      { columnField: 'title', operatorValue: 'contains', value: this.state.searchInput },
+                    ],
+                  }}
+                />
             </div>
 
         );
